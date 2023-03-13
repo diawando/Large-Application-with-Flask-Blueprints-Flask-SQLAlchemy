83e76b351865e72088fab4506e402ab3bc9b02bc
diawando <abduldiawade777@gmail.com>
2023-03-13 18:00:43 +0000

    ajout du fichier __init__.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('7f7af3a82860051b621ade99ae0cafc538193178ceb5fd70')
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:lavie@localhost:5432/social_media"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    
